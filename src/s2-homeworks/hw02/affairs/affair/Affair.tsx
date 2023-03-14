ef9d460a4a6260db7cb5169ef05e44e1d4fb234e
Vitalione <80297741401@mail.ru>
2023-03-14 21:42:41 +0300

    hw2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
+import {AffairType} from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
+    const deleteCallback = (_id: number) => {
+        props.deleteAffairCallback(props.affair._id)// need to fix
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -46,4 +46,5 @@ function Affair(props: AffairPropsType) {
     )
 }
 
-export default Affair
+
+export default Affair;
