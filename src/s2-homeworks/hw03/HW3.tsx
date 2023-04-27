fe0c89b2e207d9509e3f28cbe9769934adf41ff2
Vitalione <80297741401@mail.ru>
2023-04-27 13:26:22 +0300

    Added type UserType

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -19,20 +19,22 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
+export const pureAddUserCallback = (name: string, setUsers: (users:UserType[]) => void, users: UserType[]) => { // need to fix any
+    const user = { // need to
+        _id: v1(),
+        name: name,
     }
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
@@ -52,3 +54,4 @@ const HW3 = () => {
 }
 
 export default HW3
+//https://samurai.it-incubator.io/pc/video-content/watch/622a6c211902a6aabe6a366f
