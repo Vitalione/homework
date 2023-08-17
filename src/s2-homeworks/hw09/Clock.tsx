8526ec3c1c2368042c1d6b5fbfd666406ffef79e
Vitalione <80297741401@mail.ru>
2023-08-17 22:13:51 +0300

    add const dateFormatter

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -40,10 +40,10 @@ function Clock() {
         year: 'numeric',
     });
 
-    const stringTime = date.toLocaleTimeString('en-US', {
+    const stringTime = date.toLocaleTimeString('ru-RU', {
+        hour12: false,
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit',
     }) // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = dateFormatter.format(date) // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
