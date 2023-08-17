55996aad6f678acc808c48f646367a14a8c0f3fb
Vitalione <80297741401@mail.ru>
2023-08-17 22:00:58 +0300

    change const stringDay

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -34,13 +34,21 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' }) // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }) // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    }) // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay =  new Intl.DateTimeFormat('en-US', { weekday: 'long', month: 'long' }).format(date)// пишут студенты
-    const stringMonth = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date) // пишут студенты
+    const stringDay =  date.toLocaleDateString('en-US', { weekday: 'long' })// пишут студенты
+    const stringMonth = date.toLocaleDateString('en-US', { month: 'long' }) // пишут студенты
 
     return (
         <div className={s.clock}>
