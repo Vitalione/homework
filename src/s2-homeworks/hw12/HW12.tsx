6e6499887e1a1676f0c3c242aab6627b6e6626fc
Vitalione <80297741401@mail.ru>
2023-08-21 21:31:48 +0300

    add useSelector

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -20,10 +20,11 @@ const themes = [
 
 const HW12 = () => {
     // взять ид темы из редакса
-    const themeId = 1
+    const themeId = useSelector((state) => state.theme.themeId)
+    const dispatch = useDispatch()
 
     const change = (id: any) => { // дописать функцию
-
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -41,7 +42,9 @@ const HW12 = () => {
                     id={'hw12-select-theme'}
                     className={s.select}
                     // сделать переключение тем
-
+                    options={themes}
+                    value={themeId}
+                    onChange={change}
                 />
             </div>
         </div>
