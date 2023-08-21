c3a7340e82701f85fba4c9664006e0cd709c6509
Vitalione <80297741401@mail.ru>
2023-08-21 21:16:56 +0300

    Change const change

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,63 +1,61 @@
-import React, { useState } from 'react'
-import s from './HW11.module.css'
-import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-
-/*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+import React, { useState, useEffect } from 'react';
+import s from './HW11.module.css';
+import s2 from '../../s1-main/App.module.css';
+import { restoreState } from '../hw06/localStorage/localStorage';
+import SuperRange from './common/c7-SuperRange/SuperRange';
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value1, setValue1] = useState<number>(0);
+    const [value2, setValue2] = useState<number>(100);
+
+    useEffect(() => {
+        const storedValue1 = restoreState<number>('hw11-value1', 0);
+        const storedValue2 = restoreState<number>('hw11-value2', 100);
+
+        setValue1(storedValue1);
+        setValue2(storedValue2);
+
+        // Установка начальных значений для синхронизации слайдеров
+        if (storedValue1 !== storedValue2) {
+            setValue1(storedValue1);
+        } else {
+            setValue1(storedValue1 - 1);
+        }
+    }, []);
 
     const change = (event: React.ChangeEvent<{}> | Event, value: number | number[]) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         if (Array.isArray(value)) {
-            if (value[0] !== value1) {
-                setValue1(value[0]);
-            }
-            if (value[1] !== value2) {
-                setValue2(value[1]);
-            }
+            setValue1(value[0]);
+            setValue2(value[1]);
         } else {
             setValue1(value);
         }
-    }
+    };
 
     return (
-        <div id={'hw11'}>
+        <div id="hw11">
             <div className={s2.hwTitle}>Homework #11</div>
-
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
-                            value={value1}
-                            onChange={change}
-                        />
+            <span id="hw11-value" className={s.number}>
+              {value1}
+            </span>
+                        <SuperRange id="hw11-single-slider" value={value1} onChange={change} />
                     </div>
                     <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-                            value={[value1, value2]}
-                            onChange={change}
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+            <span id="hw11-value-1" className={s.number}>
+              {value1}
+            </span>
+                        <SuperRange id="hw11-double-slider" value={[value1, value2]} onChange={change} />
+                        <span id="hw11-value-2" className={s.number}>
+              {value2}
+            </span>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default HW11
+export default HW11;
